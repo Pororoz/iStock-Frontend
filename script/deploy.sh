39d6f6bc004933d299bc564988147709f47f234a
NaGyeong Park <ngp.july@gmail.com>
2023-01-14 19:35:20 +0900

    fix: env 관련 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-echo "> run MySQL, redis and nginx container"
-docker-compose -f docker-compose.prod.yml up mysql redis nginx -d --build
+echo "> run nginx container"
+docker-compose -f docker-compose.prod.yml up nginx -d --build
 
 echo "> docker container up"
 
 # docker up
-docker-compose -f docker-compose.prod.yml up spring1 -d --build
+docker-compose -f docker-compose.prod.yml up react -d --build
