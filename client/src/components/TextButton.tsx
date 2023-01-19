7439694c0c7befb81bd1754319444a742dcb5851
n-ryu <ababc100564@gmail.com>
2023-01-19 17:18:49 +0900

    feat: Button 컴포넌트 이름을 TextButton으로 변경

--- client/src/components/TextButton.tsx
+++ client/src/components/TextButton.tsx
@@ -1,7 +1,6 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
 import ReactProps from '@type/props';
-import Text from '@components/Text';
 
 interface Props {
   color?: string;
@@ -12,18 +11,14 @@ const StyledButton = styled.button<Props>`
   font-family: 'Noto Sans KR';
   color: var(${({ color }) => color ?? '--color-dark-gray'});
   background-color: var(${({ backgroundColor }) => backgroundColor ?? '--color-lightest-gray'});
-  padding-inline: 10px;
+  padding-inline: 12px;
   padding-block: 6px;
   border-radius: 5px;
   border: 1px solid var(${({ color }) => color ?? '--color-dark-gray'});
 `;
 
 function TextButton({ children, ...props }: ReactProps<Props>): ReactElement {
-  return (
-    <StyledButton {...props}>
-      <Text color={props.color}>{children}</Text>
-    </StyledButton>
-  );
+  return <StyledButton {...props}>{children}</StyledButton>;
 }
 
 export default TextButton;
