ca8a2b5c25188cf505b9628005b87ec8a5da744c
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2023-02-06 19:40:49 +0900

    Feat: select Input 생성
    - HTMLSelectElement ref 받아오도록 설정 및 select option 배열로 받아서 쓸 수 있도록 설정

---
+++ client/src/components/SelectInput.tsx
@@ -0,0 +1,30 @@
+import { forwardRef, ReactElement, Ref, RefObject } from 'react';
+import styled from 'styled-components';
+import Select from './Select';
+import Text from './Text';
+
+interface Props {
+  optionList: string[];
+  title: string;
+  size?: number;
+  width?: number;
+  ref: RefObject<HTMLSelectElement>;
+}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  gap: 10px;
+`;
+
+function SelectInput({ optionList, title }: Props, ref: Ref<HTMLSelectElement>): ReactElement {
+  return (
+    <Wrapper>
+      <Text size={20} weight={700}>
+        {title}
+      </Text>
+      <Select ref={ref} optionList={optionList} />
+    </Wrapper>
+  );
+}
+export default forwardRef<HTMLSelectElement, Props>(SelectInput);
