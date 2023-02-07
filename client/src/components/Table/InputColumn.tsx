b60e5f44439f698745077dd5dd859aa00d69cb8a
n-ryu <ababc100564@gmail.com>
2023-02-07 19:50:03 +0900

    feat: Input Column 설계 - input state 설정 및 props 구조 정의

--- client/src/components/Table/InputColumn.tsx
+++ client/src/components/Table/InputColumn.tsx
@@ -1,25 +1,41 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import styled from 'styled-components';
 import { ReactProps } from '@type/props';
 import TextButton from '@components/TextButton';
-import Input from '@components/Input';
 
 const Column = styled.td`
   text-align: center;
 `;
 
 interface Props {
-  onSubmit: () => void;
+  onSubmit: (input: number) => void;
 }
 
+const StyledInput = styled.input<{ color?: string; size?: number; width?: number }>`
+  font-family: 'Noto Sans KR';
+  color: var(${({ color }) => color ?? '--color-dark-gray'});
+  font-weight: 500;
+  font-size: ${({ size }) => size ?? 15}px;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  width: ${({ width }) => (width !== undefined ? `${width}px` : 'max-content')};
+`;
+
 function InputColumn({ children, onSubmit }: ReactProps<Props>): ReactElement {
+  const [input, setInput] = useState<number>(0);
   const onClick = (): void => {
-    onSubmit();
+    onSubmit(input);
   };
   return (
     <Column>
       {children}
-      <Input></Input>
+      <StyledInput
+        type="number"
+        onChange={(event) => {
+          setInput(parseInt(event.target.value));
+        }}
+      />
       <TextButton onClick={onClick}>입력</TextButton>
     </Column>
   );
