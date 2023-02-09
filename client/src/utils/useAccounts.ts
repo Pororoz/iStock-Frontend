2dc2adf64de226832d1e6c090899ebbb30b67dca
NaGyeong Park <ngp.july@gmail.com>
2023-02-09 20:30:42 +0900

    feat: AccountApi Data Type 정의

--- client/src/utils/useAccounts.ts
+++ client/src/utils/useAccounts.ts
@@ -1,30 +1,60 @@
 import axios, { AxiosResponse } from 'axios';
 
-export interface AccountResponse {
+export interface AccountResponse<T> {
   status: number;
   message?: string;
-  data: any;
+  data: T;
+}
+
+interface AccountInfoData {
+  totalPages: number;
+  totalElements: number;
+  currentSize: number;
+  first: boolean;
+  last: boolean;
+  contents: ServerAccountContent[];
+}
+
+interface ClientAccountContent {
+  userId: number;
+  username: string;
+  roleName: 'ROLE_USER' | 'ROLE_ADMIN';
+  createdAt: Date;
+  updatedAt: Date;
+}
+interface ServerAccountContent {
+  userId: number;
+  username: string;
+  roleName: 'ROLE_USER' | 'ROLE_ADMIN';
+  createdAt: string;
+  updatedAt: string;
 }
 
 interface AccountType {
-  id?: number;
-  username?: string;
-  password?: string;
+  userId?: number;
   roleName?: string;
+  username?: string;
 }
 
-export const getUsers = async (): Promise<AxiosResponse<AccountResponse, any>> => {
-  return await axios.get<AccountResponse>('/users');
+export const getUsers = async (): Promise<AxiosResponse<AccountResponse<AccountInfoData>, any>> => {
+  return await axios.get<AccountResponse<AccountInfoData>>('/users');
 };
 
-export const createUser = async (parameter: AccountType): Promise<AxiosResponse<AccountResponse, any>> => {
-  return await axios.post<AccountResponse>('/users', parameter);
+export const createUser = async (parameter: AccountType): Promise<AxiosResponse<AccountResponse<AccountType>>> => {
+  return await axios.post<AccountResponse<AccountType>>('/users', parameter);
 };
 
-export const updateUser = async (parameter: AccountType): Promise<AxiosResponse<AccountResponse, any>> => {
-  return await axios.put<AccountResponse>('/users', parameter);
+export const updateUser = async (parameter: AccountType): Promise<AxiosResponse<AccountResponse<AccountType>>> => {
+  return await axios.put<AccountResponse<AccountType>>('/users', parameter);
 };
 
-export const deleteUser = async (id: string): Promise<AxiosResponse<AccountResponse, any>> => {
-  return await axios.delete<AccountResponse>(`/users/${id === undefined ? '' : id.toString()}`);
+export const deleteUser = async (id: string): Promise<AxiosResponse<AccountResponse<AccountType>>> => {
+  return await axios.delete<AccountResponse<AccountType>>(`/users/${id === undefined ? '' : id.toString()}`);
+};
+
+export const handleOnSuccess = (data: AxiosResponse<AccountResponse<AccountInfoData>>): ClientAccountContent[] => {
+  const newData = data.data.data.contents.map((elem: ServerAccountContent) => {
+    return { ...elem, createdAt: new Date(elem.createdAt), updatedAt: new Date(elem.updatedAt) };
+  });
+  return newData;
 };
