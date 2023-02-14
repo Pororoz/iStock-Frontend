9dabec3c34a1dc50a58b386458b10d3bac34a5bd
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2023-02-14 22:04:13 +0900

    Feat: Suspense 일반화 및 account modal 연결

--- client/src/pages/AccountsPage.tsx
+++ client/src/pages/AccountsPage.tsx
@@ -1,18 +1,44 @@
 import AccountTable from '@components/Tables/AccountTable';
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import { useQuery } from 'react-query';
 import { handleOnError } from '@utils/common';
 import { getUsers, transformData } from '@utils/useAccounts';
+import SideButton from '@components/SideButton';
+import AuthModal from '@components/AuthModal';
+import { AccountDtoType } from '@type/dto.type';
 
 function AccountsPage(): ReactElement {
-  const { data, isLoading } = useQuery('users', getUsers, {
+  const [hasModal, setHasModal] = useState(false);
+  const [target, setTarget] = useState<AccountDtoType>(); // 수정 시 모달에 표현돼야 하는 데이터
+
+  const { data } = useQuery('users', getUsers, {
     onError: handleOnError,
     select: (data) => transformData(data),
   });
 
+  const openEditModal = (row: AccountDtoType): void => {
+    setHasModal(true);
+    setTarget(row);
+  };
+
+  const openModal = (): void => {
+    setHasModal(true);
+  };
+
+  const closeModal = (): void => {
+    setHasModal(false);
+    setTarget(undefined);
+  };
+
   return (
     <div>
-      {isLoading ? <div>loading</div> : data !== undefined ? <AccountTable rows={data} /> : <div>dataundefined</div>}
+      {data !== undefined && (
+        <div>
+          <AccountTable rows={data} updateAction={openEditModal} />
+          <SideButton action={openModal} />
+          {hasModal && <AuthModal close={closeModal} target={target} />}
+        </div>
+      )}
     </div>
   );
 }
