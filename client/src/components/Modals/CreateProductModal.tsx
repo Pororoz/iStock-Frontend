e169a66c69ac065c4d90bf6b9b2625ed9dcae646
n-ryu <ababc100564@gmail.com>
2023-02-22 02:53:23 +0900

    feat: CreateProductModal 구현

--- client/src/components/Modals/CreateProductModal.tsx
+++ client/src/components/Modals/CreateProductModal.tsx
@@ -6,6 +6,7 @@ import useMutate from '@hooks/useMutate';
 import { createCategory } from '@fetches/category';
 import { useParams } from 'react-router-dom';
 import useModalInput from '@hooks/useModalInput';
+import { required, lengthValidator } from '@utils/validator';
 
 interface Props {
   onSubmit?: () => void;
@@ -17,10 +18,10 @@ export default function CreateProductModal({ onClose = () => {} }: Props): React
   const { mutate } = useMutate({ key: 'products', action: createCategory, onSuccess: onClose });
   const { category } = useParams();
   const values = {
-    productName: useModalInput([]),
-    productNumber: useModalInput([]),
+    productName: useModalInput([required, lengthValidator(2, 50)]),
+    productNumber: useModalInput([required, lengthValidator(2, 50)]),
     codeNumber: useModalInput([]),
-    stock: useModalInput([], 0),
+    stock: useModalInput([], '0'),
     companyName: useModalInput([]),
     categoryId: useModalInput([], category),
   };
@@ -34,14 +35,35 @@ export default function CreateProductModal({ onClose = () => {} }: Props): React
       onClose={onClose}
     >
       <Text>제품 생성</Text>
-      <ModalInput value={values.productName.value as string} title="품명" onChange={values.productName.onChange} />
-      <ModalInput value={values.productNumber.value as string} title="품번" onChange={values.productNumber.onChange} />
-      <ModalInput value={values.codeNumber.value as string} title="제품코드" onChange={values.codeNumber.onChange} />
-      <ModalInput value={values.stock.value as string} title="재고" onChange={values.stock.onChange} />
+      <ModalInput
+        value={values.productName.value as string}
+        title="품명"
+        onChange={values.productName.onChange}
+        errorMessage={values.productName.errorMessage}
+      />
+      <ModalInput
+        value={values.productNumber.value as string}
+        title="품번"
+        onChange={values.productNumber.onChange}
+        errorMessage={values.productNumber.errorMessage}
+      />
+      <ModalInput
+        value={values.codeNumber.value as string}
+        title="제품코드"
+        onChange={values.codeNumber.onChange}
+        errorMessage={values.codeNumber.errorMessage}
+      />
+      <ModalInput
+        value={values.stock.value as string}
+        title="재고"
+        onChange={values.stock.onChange}
+        errorMessage={values.stock.errorMessage}
+      />
       <ModalInput
         value={values.companyName.value as string}
         title="거래처 이름"
         onChange={values.companyName.onChange}
+        errorMessage={values.companyName.errorMessage}
       />
       <ModalInput value={values.categoryId.value as string} title="카테고리" readonly />
     </Modal>
